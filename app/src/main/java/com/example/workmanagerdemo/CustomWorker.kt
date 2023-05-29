6b787df314938968ccd20350f563b10aa5f048ba
tmromao <tiagomromao@gmail.com>
2023-05-29 22:05:11 +0100

    working app. downloading a post in the background

--- app/src/main/java/com/example/workmanagerdemo/CustomWorker.kt
+++ app/src/main/java/com/example/workmanagerdemo/CustomWorker.kt
@@ -1,17 +1,40 @@
 package com.example.workmanagerdemo
 
 import android.content.Context
+import android.util.Log
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 
-class CustomWorker(
-    context: Context,
-    workerParameters: WorkerParameters
-): Worker(context,workerParameters)
-{
-    override fun doWork(): Result {
-        println("Hello from Worker!")
-        return Result.success()
+@HiltWorker
+class CustomWorker @AssistedInject constructor(
+    @Assisted private val api: DemoApi,
+    @Assisted context: Context,
+    @Assisted workerParameters: WorkerParameters
+) : CoroutineWorker(context, workerParameters) {
+    override suspend fun doWork(): Result {
+        /*println("Hello from Worker!")
+        return Result.success()*/
+        return try {
+            val response = api.getPost()
+            if (response.isSuccessful) {
+                Log.d(
+                    "CustomWorker",
+                    "doWork: ${response.body()?.id} Title: ${response.body()?.title}"
+                )
+                Result.success()
+            } else {
+                Log.d("CustomWorker", "Retrying... ")
+                Result.retry()
+            }
+        } catch (e: Exception) {
+            Log.d("CustomWorker", "Error!... ")
+            Result.failure(Data.Builder().putString("Error", e.toString()).build())
+        }
+
     }
-
 }
--- app/src/main/java/com/example/workmanagerdemo/MyApplication.kt
+++ app/src/main/java/com/example/workmanagerdemo/MyApplication.kt
@@ -1,7 +1,33 @@
 package com.example.workmanagerdemo
 
 import android.app.Application
+import android.content.Context
+import android.util.Log
+import androidx.work.Configuration
+import androidx.work.ListenableWorker
+import androidx.work.WorkerFactory
+import androidx.work.WorkerParameters
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MyApplication : Application()
+class MyApplication : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: CustomWorkerFactory
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setMinimumLoggingLevel(Log.DEBUG)
+            .setWorkerFactory(workerFactory)
+            .build()
+    }
+}
+
+class CustomWorkerFactory @Inject constructor(private val api: DemoApi) : WorkerFactory() {
+    override fun createWorker(
+        appContext: Context,
+        workerClassName: String,
+        workerParameters: WorkerParameters
+    ): ListenableWorker = CustomWorker(api, appContext, workerParameters)
+
+}
